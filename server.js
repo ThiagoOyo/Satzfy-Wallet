a706721b7d6768e36ecb0ed84332e52b5b1fe6b8
thgOyo <thiagooyo@gmail.com>
2024-04-01 20:35:05 -0300

    feat: Adding HTTPS configuration

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const { connect, grpc } = require('./lnd.js');
 const cors = require('cors');
 const lightningRouter = require("./lightning/lightningRouter.js")
+const dotenv = require('dotenv');
+const fs = require('fs');
+const https = require('https');
 
+dotenv.config();
 const server = express();
 
 server.use(express.json());
@@ -16,5 +20,16 @@ server.get("/", (req, res) => {
 });
 
 server.use("/lightning", lightningRouter);
+
+const options = {
+    key: fs.readFileSync(process.env.HTTPS_KEY), 
+    cert: fs.readFileSync(process.env.HTTPS_CERT),
+    passphrase: process.env.HTTPS_PASSPHRASE
+};
+
+https.createServer(options, server).listen(443, () => {
+    console.log('Servidor HTTPS rodando na porta 443');
+});
+
 
 module.exports = server;
